01d4c266b8908810c7140a0a9da99646e9f3c395
bidyutdas@laser <bidyutdas.laserpowerinfra@gmail.com>
2026-07-28 15:16:21 +0530

    [UPDATE] new schema

---
+++ v2/prisma/migrations/20260728092652_proposederpquantity/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "tender_merged" ADD COLUMN     "contractNo" TEXT,
+ADD COLUMN     "ourRank" TEXT,
+ADD COLUMN     "ourValue" TEXT,
+ADD COLUMN     "proposedErpQuantity" TEXT;
